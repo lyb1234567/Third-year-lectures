812741c685c8e4d1a7524df0bdfeaf693ab75f14
Dixon <1536635817@qq.com>
2022-02-21 13:22:06 +0000

    fix

--- EEE380/Algorithm/test.m
+++ EEE380/Algorithm/test.m
@@ -1,12 +1,11 @@
 
-Pmax=0.2;
 Pkc=0.1995262315;
  hkc=2.707*10^(-6);
  hki=3.7429*10^(-7);
  hiD=1.1527*10^(-4);
  hiB=4.9154*10^(-7);
- k=0.6967;
- b=-19.1737*10^(-6);
+ k=0.1427;
+ b=108.2778*10^(-6);
  
  Pth=[10 100 230.06 57368]*10^(-6);
  Tmin_D=2;
@@ -14,29 +13,26 @@ Tmin_C=1;
  phi=0;
  s=1*10^(-5);
  PiD_initial=random(0,0.2);
- Q_=[];
-%  for i=1:2
-%      Pmax=P(i);
-sum_EE=[];
-% for p=1:2
-iteration=[];
 EE_array=[];
-temp=[];
-% Pmax=P(p);
 QiD=10;
- alpha=random(0,1);
- beta=random(0,1);
- gamma=random(0,1);
- delta=random(0,1);
- in=random(0,1);
-
- I=1;
- Q=[];
- t=1;
- lambda=lambda_fix_PiD(PiD_initial,alpha,beta,gamma,delta,in,hiD,hki,k,QiD);
  P=[0.1 0.1995262315];
- QiD=random(0,10);
-I=1;
+I=10;
+number=1000;
+
+for i=1:2
+Pmax=P(i);
+Q_sum=[];
+Q=[];
+alpha=random(0,1);
+    beta=random(0,1);
+    gamma=random(0,1);
+    delta=random(0,1);
+    in=random(0,1);
+for f=1:number
+    QiD=10;
+    t=1;
+    lambda=lambda_fix_PiD(PiD_initial,alpha,beta,gamma,delta,in,hiD,hki,k,QiD);
+     I=10;
  while t<=I
      PiD=PiD_fix_lambda(lambda,alpha,beta,gamma,delta,in,hiD,hki,hkc,hiB,k,QiD);
      lambda=lambda_fix_PiD(PiD,alpha,beta,gamma,delta,in,hiD,hki,k,QiD);
@@ -45,9 +41,8 @@ I=1;
      PiR=received_power(lambda, PiD,hiD,hki);
      [EH,j]=EH_model(PiR);
      EC=Energy_Consumption(PiD,EH);
-     Q(t)=QiD;
-     T_D-QiD*EC
-     if T_D-QiD*EC>=phi
+     Q(f,t)=QiD;
+     if T_D-QiD*EC>phi
           QiD=T_D/EC;
           alpha_=alpha+s*(PiD-Pmax);
           alpha=max([0 alpha_]);
@@ -63,30 +58,30 @@ I=1;
           
           in_=in-s*(PiR-Pth(1));
           in=max([0 in_]);
-          continue;
      else
          PiD_new=PiD;
          lambda_new=lambda;
          EE=QiD;
-         Q(t)=QiD;
+         Q(f,t)=QiD;
+         for n=t+1:I
+             Q(f,n)=QiD;
+         end
+         break;
      end 
      t=t+1;
  end
-
-% sum_EE(p,:)=sum(temp);
-% end
-% sum_EE=sum_EE./10;
-% iteration=[];
-% for i=1:I
-%     iteration(end+1)=i;
-% end
-% plot(iteration,sum_EE(1,:),'--');
-% hold on
-% plot(iteration,sum_EE(2,:),'-');
-% plot(iteration,EE_array,'-');
-% title('Convergence of inner loop Algorithm');
-% xlabel('interation step')
-% ylabel('Energy Efficiency [bits/Hz/J]');
-% legend('Pmax=20dBm','Pmax=23dBm');
+ Q_sum=sum(Q)./number;
+end
+ EE_array(i,:)=Q_sum;
+end
+iteration=[];
+for i=1:size(Q,2)
+    iteration(end+1)=i;
+end
+plot(iteration,EE_array(1,:),iteration,EE_array(2,:));
+xlabel('interation step')
+ylabel('Energy Efficiency [bits/Hz/J]');
+ylim([0 50]);
+legend('Pmax=20dBm','Pmax=23dBm');
 
  
