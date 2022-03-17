14c54d33eaae867e2f35f3e0aed79ee274bb13b8
Dixon <1536635817@qq.com>
2022-03-17 08:09:50 +0000

    fix

--- EEE380/Algorithm/PiD_comparison.m
+++ EEE380/Algorithm/PiD_comparison.m
@@ -6,7 +6,7 @@ function PiD_optimal=PiD_comparison(beta,gamma,QiD,delta_I,delta_E,hiD,hki,Pkc)
  A=eta*(1+gamma)*(1/log(2));
  B=QiD*(1-eta*delta_E*eta_E*hiD)+eta*beta;
  C=delta_I*(Pkc*hki+N0)+N1;
- D=delta_I*hiD^(2);
+ D=delta_I*hiD;
  
  PiD_optimal=(A/B)-(C/D);
 end
